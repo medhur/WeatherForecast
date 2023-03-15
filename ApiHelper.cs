f61f1db2a4fc7b0b5de31d5135958d9cf6e9a16b
meena.dhurshetty <meena.dhurshetty@speridian.com>
2023-03-15 20:51:27 +0530

    Add project files.

---
+++ ApiHelper.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using WeatherReport.Models;
+
+namespace WeatherForecast
+{
+    public static class ApiHelper
+    {
+        public static async Task<WeatherReportViewtModel> GetAsync(WeatherReportViewtModel weatherModel)
+        {
+            //TODO: Get this from app settings instead of hard coding.
+            string appId = "8113fcc5a7494b0518bd91ef3acc074f";
+
+            //TODO: Get this from app settings instead of hard coding.
+            string url = "http://api.openweathermap.org/data/2.5/";
+
+            string weatherApi = string.Format("weather?q={0}&units=Celsius&APPID={1}", weatherModel.Country, appId);
+
+            using var client = new HttpClient();
+            {
+                client.BaseAddress = new Uri(url);
+                // Add an Accept header for JSON format.
+                client.DefaultRequestHeaders.Accept.Add(
+                   new MediaTypeWithQualityHeaderValue("application/json"));
+                // Get data response
+                var response = await client.GetAsync(weatherApi);
+                if (response.IsSuccessStatusCode)
+                {
+                    weatherModel = JsonConvert.DeserializeObject<WeatherReportViewtModel>(response.Content.ReadAsStringAsync().Result);
+                }
+                else
+                {
+                    //TODO: Log the exception in log file.
+                }
+            }
+
+            return weatherModel;
+        }
+    }
+}
---
+++ Controllers/WeatherController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.Xml;
+using System.Threading.Tasks;
+using WeatherReport.Models;
+
+namespace WeatherForecast.Controllers
+{
+    public class WeatherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GetWeatherInfo(WeatherReportViewtModel weatherModel)
+        {
+            try
+            {
+                // call the weather API to get the information.
+                weatherModel = ApiHelper.GetAsync(weatherModel)?.Result;
+
+                weatherModel.ShowWeatherCard = true;
+                return View("Index", weatherModel);
+            }
+            catch (Exception ex)
+            {
+                // TODO: Log the stack trace in log file.
+                return View("Error");
+            }
+
+        }
+    }
+}
